a89a889a1babf0ab104f6538e041eed08c12b2ed
Oscar Grainger <oscarvgrainger@gmail.com>
2017-10-06 15:53:35 +0200

    updated for jpa

--- src/main/java/com/ynov/banqueEL/model/Client.java
+++ src/main/java/com/ynov/banqueEL/model/Client.java
@@ -1,54 +1,19 @@
 package com.ynov.banqueEL.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Client {
 
+    @Id
+    private int clientID;
     private String firstName;
     private String lastName;
-    private int clientID;
     private String password;
     private String login;
 
     public String toString() {
         return firstName + ' ' + lastName + ' ' + clientID + ' ' + login;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String n) {
-        firstName = n;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String n) {
-        lastName = n;
-    }
-
-    public int getClientID() {
-        return clientID;
-    }
-
-    public void setClientID(int n) {
-        clientID = n;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String n) {
-        password = n;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String n) {
-        login = n;
     }
 }
